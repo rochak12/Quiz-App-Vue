20a0369551d75281dba90b50d215b2779ea62f65
Rochak <rochak.kunwar.rk@gmail.com>
2022-04-22 16:43:10 -0500

    final-complete quiz app

---
+++ src/components/Result.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="result">
+    <div class="title">{{ results[resultIndex].title }}</div>
+    <div class="desc">
+      {{ results[resultIndex].desc }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quiz-result",
+  props: ["results", "totalCorrect"],
+  computed: {
+    resultIndex() {
+      let index = 0;
+
+      this.results.forEach((element, i) => {
+        if (
+          element.min <= this.totalCorrect &&
+          this.totalCorrect >= element.max
+        ) {
+          index = i;
+        }
+      });
+
+      return index;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
